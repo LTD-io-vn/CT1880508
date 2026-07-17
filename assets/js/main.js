1f5ed5996272f1ffedb4a8e7e872c9251c856ebf
LTD-io-vn <contact@ltd.io.vn>
2026-07-18 01:15:15 +0700

    Change name of several items

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,31 +22,31 @@ navBar.addEventListener("click", () => navToggle.click());
 
 const productData = {
 	SP0000: {
-		name: "Sơn Tùng M-TP - Chúng Ta Của Tương Lai",
+		name: "Chúng Ta Của Tương Lai",
 		origin: "Sơn Tùng M-TP",
 		description: "Đĩa than phiên bản sưu tầm của ca khúc Chúng Ta Của Tương Lai với chất lượng âm thanh cao.",
 		price: 1250000,
-		image: "/assets/imgs/sanpham/vinyl/chung-ta-cua-tuong-lai.jpg",
+		image: "/assets/imgs/vinyl.jpg",
 		type: "Đĩa than"
 	},
 	SP0001: {
-		name: "Sơn Tùng M-TP - Đừng Làm Trái Tim Anh Đau",
+		name: "Đừng Làm Trái Tim Anh Đau",
 		origin: "Sơn Tùng M-TP",
 		description: "Phiên bản vinyl của bản hit Đừng Làm Trái Tim Anh Đau dành cho người hâm mộ.",
 		price: 1190000,
-		image: "/assets/imgs/sanpham/vinyl/dung-lam-trai-tim-anh-dau.jpg",
+		image: "/assets/imgs/vinyl.jpg",
 		type: "Đĩa than"
 	},
 	SP0002: {
-		name: "Sơn Tùng M-TP - Hãy Trao Cho Anh",
+		name: "Hãy Trao Cho Anh",
 		origin: "Sơn Tùng M-TP",
 		description: "Đĩa than chất lượng cao với ca khúc nổi tiếng Hãy Trao Cho Anh.",
 		price: 1290000,
-		image: "/assets/imgs/sanpham/vinyl/hay-trao-cho-anh.jpg",
+		image: "/assets/imgs/vinyl.jpg",
 		type: "Đĩa than"
 	},
 	SP0003: {
-		name: "Sơn Tùng M-TP - Chạy Ngay Đi",
+		name: "Chạy Ngay Đi",
 		origin: "Sơn Tùng M-TP",
 		description: "Phiên bản vinyl giới hạn của Chạy Ngay Đi với thiết kế bìa độc quyền.",
 		price: 1150000,
@@ -54,7 +54,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0004: {
-		name: "Sơn Tùng M-TP - Lạc Trôi",
+		name: "Lạc Trôi",
 		origin: "Sơn Tùng M-TP",
 		description: "Đĩa than Lạc Trôi mang đến trải nghiệm âm thanh chân thực.",
 		price: 1220000,
@@ -62,7 +62,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0005: {
-		name: "Tyler, The Creator - Chromakopia",
+		name: "Chromakopia",
 		origin: "Tyler, The Creator",
 		description: "Album phòng thu thứ tám của Tyler, The Creator phát hành năm 2024.",
 		price: 1300000,
@@ -70,7 +70,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0006: {
-		name: "Taylor Swift - 1989 (Taylor's Version)",
+		name: "(Taylor's Version)",
 		origin: "Taylor Swift",
 		description: "Phiên bản tái thu âm của album pop nổi tiếng 1989.",
 		price: 1450000,
@@ -78,7 +78,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0007: {
-		name: "Taylor Swift - Midnights",
+		name: "Midnights",
 		origin: "Taylor Swift",
 		description: "Album Midnights với những bản pop hiện đại và sâu lắng.",
 		price: 1380000,
@@ -86,7 +86,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0008: {
-		name: "Adele - 30",
+		name: "30",
 		origin: "Adele",
 		description: "Album 30 của Adele với chất giọng đầy cảm xúc.",
 		price: 1350000,
@@ -94,7 +94,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0009: {
-		name: "Bruno Mars - 24K Magic",
+		name: "Magic",
 		origin: "Bruno Mars",
 		description: "Album funk-pop nổi tiếng đạt nhiều giải thưởng Grammy.",
 		price: 1280000,
@@ -102,7 +102,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0010: {
-		name: "Daft Punk - Random Access Memories",
+		name: "Access Memories",
 		origin: "Daft Punk",
 		description: "Album kinh điển với ca khúc Get Lucky.",
 		price: 1500000,
@@ -110,7 +110,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0011: {
-		name: "Michael Jackson - Thriller",
+		name: "Thriller",
 		origin: "Michael Jackson",
 		description: "Một trong những album bán chạy nhất mọi thời đại.",
 		price: 1550000,
@@ -118,7 +118,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0012: {
-		name: "Queen - Greatest Hits",
+		name: "Hits",
 		origin: "Queen",
 		description: "Tuyển tập những ca khúc nổi tiếng nhất của Queen.",
 		price: 1480000,
@@ -126,7 +126,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0013: {
-		name: "Pink Floyd - The Dark Side of the Moon",
+		name: "Dark Side of the Moon",
 		origin: "Pink Floyd",
 		description: "Album rock huyền thoại với chất lượng thu âm xuất sắc.",
 		price: 1600000,
@@ -134,7 +134,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0014: {
-		name: "The Beatles - Abbey Road",
+		name: "Road",
 		origin: "The Beatles",
 		description: "Album cuối cùng được ban nhạc thu âm cùng nhau.",
 		price: 1580000,
@@ -142,7 +142,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0015: {
-		name: "Nirvana - Nevermind",
+		name: "Nevermind",
 		origin: "Nirvana",
 		description: "Album grunge kinh điển của thập niên 90.",
 		price: 1420000,
@@ -150,7 +150,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0016: {
-		name: "Coldplay - Parachutes",
+		name: "Parachutes",
 		origin: "Coldplay",
 		description: "Album đầu tay với bản hit Yellow.",
 		price: 1330000,
@@ -158,7 +158,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0017: {
-		name: "Ed Sheeran - Divide",
+		name: "Divide",
 		origin: "Ed Sheeran",
 		description: "Album Divide với nhiều ca khúc đình đám.",
 		price: 1290000,
@@ -166,7 +166,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0018: {
-		name: "The Weeknd - After Hours",
+		name: "Hours",
 		origin: "The Weeknd",
 		description: "Album synth-pop nổi bật với Blinding Lights.",
 		price: 1370000,
@@ -174,7 +174,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0019: {
-		name: "Billie Eilish - Happier Than Ever",
+		name: "Than Ever",
 		origin: "Billie Eilish",
 		description: "Album thứ hai của Billie Eilish với nhiều bản hit.",
 		price: 1390000,
@@ -182,7 +182,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0020: {
-		name: "Lana Del Rey - Born to Die",
+		name: "to Die",
 		origin: "Lana Del Rey",
 		description: "Album mang phong cách dream pop đặc trưng.",
 		price: 1410000,
@@ -190,7 +190,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0021: {
-		name: "Arctic Monkeys - AM",
+		name: "AM",
 		origin: "Arctic Monkeys",
 		description: "Album rock hiện đại được yêu thích trên toàn thế giới.",
 		price: 1360000,
@@ -198,7 +198,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0022: {
-		name: "Imagine Dragons - Evolve",
+		name: "Evolve",
 		origin: "Imagine Dragons",
 		description: "Album với các ca khúc Believer và Thunder.",
 		price: 1320000,
@@ -206,7 +206,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0023: {
-		name: "Linkin Park - Hybrid Theory",
+		name: "Theory",
 		origin: "Linkin Park",
 		description: "Album đầu tay mang tính biểu tượng của Linkin Park.",
 		price: 1460000,
@@ -214,7 +214,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0024: {
-		name: "Eagles - Hotel California",
+		name: "California",
 		origin: "Eagles",
 		description: "Album rock cổ điển với ca khúc Hotel California bất hủ.",
 		price: 1520000,
@@ -222,7 +222,7 @@ const productData = {
 		type: "Đĩa than"
 	},
 	SP0025: {
-		name: "Audio-Technica AT-LP60X",
+		name: "AT-LP60X",
 		origin: "Audio-Technica",
 		description: "Mâm đĩa than truyền động dây đai hoàn toàn tự động, tích hợp preamp phono, phù hợp cho người mới bắt đầu.",
 		price: 4900000,
@@ -230,7 +230,7 @@ const productData = {
 		type: "Mâm đĩa than"
 	},
 	SP0026: {
-		name: "Audio-Technica AT-LPW30TK",
+		name: "AT-LPW30TK",
 		origin: "Audio-Technica",
 		description: "Mâm đĩa than thủ công với mặt gỗ MDF, truyền động dây đai và khả năng nâng cấp kim đọc.",
 		price: 7900000,
@@ -238,7 +238,7 @@ const productData = {
 		type: "Mâm đĩa than"
 	},
 	SP0027: {
-		name: "Sony PS-LX310BT",
+		name: "PS-LX310BT",
 		origin: "Sony",
 		description: "Mâm đĩa than tự động hỗ trợ kết nối Bluetooth, dễ dàng kết nối với loa và tai nghe không dây.",
 		price: 7200000,
@@ -246,7 +246,7 @@ const productData = {
 		type: "Mâm đĩa than"
 	},
 	SP0028: {
-		name: "Pro-Ject Debut Carbon EVO",
+		name: "Debut Carbon EVO",
 		origin: "Pro-Ject",
 		description: "Mâm đĩa than cao cấp với cần carbon, chống rung hiệu quả và chất lượng âm thanh vượt trội.",
 		price: 16500000,
@@ -254,7 +254,7 @@ const productData = {
 		type: "Mâm đĩa than"
 	},
 	SP0029: {
-		name: "Rega Planar 1",
+		name: "Planar 1",
 		origin: "Rega",
 		description: "Mâm đĩa than thủ công nổi tiếng với thiết kế tối giản và khả năng tái tạo âm thanh trung thực.",
 		price: 11900000,
@@ -262,7 +262,7 @@ const productData = {
 		type: "Mâm đĩa than"
 	},
 	SP0030: {
-		name: "Denon DP-300F",
+		name: "DP-300F",
 		origin: "Denon",
 		description: "Mâm đĩa than tự động với preamp tích hợp, phù hợp cho hệ thống âm thanh gia đình.",
 		price: 9800000,
@@ -270,7 +270,7 @@ const productData = {
 		type: "Mâm đĩa than"
 	},
 	SP0031: {
-		name: "Fluance RT82",
+		name: "RT82",
 		origin: "Fluance",
 		description: "Mâm đĩa than truyền động dây đai với cảm biến tốc độ quang học cho độ chính xác cao.",
 		price: 9300000,
@@ -278,7 +278,7 @@ const productData = {
 		type: "Mâm đĩa than"
 	},
 	SP0032: {
-		name: "Technics SL-1200MK7",
+		name: "SL-1200MK7",
 		origin: "Technics",
 		description: "Mâm đĩa than truyền động trực tiếp chuyên nghiệp, được các DJ trên toàn thế giới tin dùng.",
 		price: 28900000,
@@ -286,7 +286,7 @@ const productData = {
 		type: "Mâm đĩa than"
 	},
 	SP0033: {
-		name: "Pioneer DJ PLX-500",
+		name: "PLX-500",
 		origin: "Pioneer DJ",
 		description: "Mâm đĩa than direct-drive dành cho DJ và người yêu nhạc với khả năng ghi âm qua USB.",
 		price: 9800000,
@@ -294,7 +294,7 @@ const productData = {
 		type: "Mâm đĩa than"
 	},
 	SP0034: {
-		name: "Victrola Eastwood",
+		name: "Eastwood",
 		origin: "Victrola",
 		description: "Mâm đĩa than phong cách cổ điển tích hợp Bluetooth và loa, thích hợp cho không gian gia đình.",
 		price: 4200000,
@@ -302,7 +302,7 @@ const productData = {
 		type: "Mâm đĩa than"
 	},
 	SP0035: {
-		name: "Edifier R1280DB",
+		name: "R1280DB",
 		origin: "Edifier",
 		description: "Loa bookshelf tích hợp Bluetooth với chất âm cân bằng, phù hợp nghe nhạc vinyl và giải trí tại gia.",
 		price: 2890000,
@@ -310,7 +310,7 @@ const productData = {
 		type: "Loa"
 	},
 	SP0036: {
-		name: "Marshall Acton III",
+		name: "Acton III",
 		origin: "Marshall",
 		description: "Loa Bluetooth thiết kế cổ điển với âm thanh mạnh mẽ, thích hợp cho phòng khách hoặc phòng làm việc.",
 		price: 7490000,
@@ -318,7 +318,7 @@ const productData = {
 		type: "Loa"
 	},
 	SP0037: {
-		name: "Audioengine A2+",
+		name: "A2+",
 		origin: "Audioengine",
 		description: "Loa bookshelf nhỏ gọn hỗ trợ Bluetooth, mang đến âm thanh chi tiết và trung thực.",
 		price: 6590000,
@@ -326,7 +326,7 @@ const productData = {
 		type: "Loa"
 	},
 	SP0038: {
-		name: "Klipsch The Fives",
+		name: "The Fives",
 		origin: "Klipsch",
 		description: "Loa hi-fi cao cấp với kết nối đa dạng và chất âm sống động dành cho người yêu nhạc.",
 		price: 18900000,
@@ -334,7 +334,7 @@ const productData = {
 		type: "Loa"
 	},
 	SP0039: {
-		name: "JBL Authentics 300",
+		name: "Authentics 300",
 		origin: "JBL",
 		description: "Loa không dây phong cách retro với âm thanh mạnh mẽ và thời lượng pin dài.",
 		price: 10900000,
@@ -342,7 +342,7 @@ const productData = {
 		type: "Loa"
 	},
 	SP0040: {
-		name: "Sony SRS-RA3000",
+		name: "SRS-RA3000",
 		origin: "Sony",
 		description: "Loa không dây hỗ trợ âm thanh 360 Reality Audio, mang lại trải nghiệm nghe nhạc sống động.",
 		price: 6990000,
@@ -350,7 +350,7 @@ const productData = {
 		type: "Loa"
 	},
 	SP0041: {
-		name: "Harman Kardon Aura Studio 4",
+		name: "Kardon Aura Studio 4",
 		origin: "Harman Kardon",
 		description: "Loa Bluetooth thiết kế trong suốt độc đáo, kết hợp âm thanh chất lượng cao và vẻ đẹp hiện đại.",
 		price: 7990000,
@@ -358,7 +358,7 @@ const productData = {
 		type: "Loa"
 	},
 	SP0042: {
-		name: "Bose Home Speaker 500",
+		name: "Home Speaker 500",
 		origin: "Bose",
 		description: "Loa thông minh với âm thanh stereo rộng, hỗ trợ điều khiển bằng giọng nói và Bluetooth.",
 		price: 9990000,
@@ -366,7 +366,7 @@ const productData = {
 		type: "Loa"
 	},
 	SP0043: {
-		name: "KEF LSX II",
+		name: "LSX II",
 		origin: "KEF",
 		description: "Cặp loa không dây hi-fi nhỏ gọn với chất âm chi tiết và khả năng kết nối hiện đại.",
 		price: 29900000,
@@ -374,7 +374,7 @@ const productData = {
 		type: "Loa"
 	},
 	SP0044: {
-		name: "Fluance Ai61",
+		name: "Ai61",
 		origin: "Fluance",
 		description: "Loa bookshelf chủ động hỗ trợ Bluetooth aptX HD, lý tưởng để kết hợp cùng mâm đĩa than.",
 		price: 10990000,
@@ -382,7 +382,7 @@ const productData = {
 		type: "Loa"
 	},
 	SP0045: {
-		name: "Yamaha A-S301",
+		name: "A-S301",
 		origin: "Yamaha",
 		description: "Âm ly tích hợp mang đến âm thanh trong trẻo, công suất ổn định, phù hợp với hệ thống nghe nhạc gia đình.",
 		price: 10990000,
@@ -390,7 +390,7 @@ const productData = {
 		type: "Amply"
 	},
 	SP0046: {
-		name: "Denon PMA-600NE",
+		name: "PMA-600NE",
 		origin: "Denon",
 		description: "Âm ly stereo tích hợp Bluetooth với chất âm chi tiết, lý tưởng để phối ghép cùng mâm đĩa than.",
 		price: 12990000,
@@ -398,7 +398,7 @@ const productData = {
 		type: "Amply"
 	},
 	SP0047: {
-		name: "Marantz PM6007",
+		name: "PM6007",
 		origin: "Marantz",
 		description: "Âm ly hi-fi nổi tiếng với âm thanh ấm áp, hỗ trợ ngõ vào phono dành cho người yêu đĩa than.",
 		price: 15990000,
@@ -406,7 +406,7 @@ const productData = {
 		type: "Amply"
 	},
 	SP0048: {
-		name: "Cambridge Audio AXA35",
+		name: "Audio AXA35",
 		origin: "Cambridge Audio",
 		description: "Âm ly tích hợp thiết kế tối giản, tích hợp phono MM, mang lại trải nghiệm nghe nhạc tự nhiên.",
 		price: 11990000,
@@ -414,7 +414,7 @@ const productData = {
 		type: "Amply"
 	},
 	SP0049: {
-		name: "Onkyo A-9110",
+		name: "A-9110",
 		origin: "Onkyo",
 		description: "Âm ly stereo hiệu suất cao với âm thanh mạnh mẽ, phù hợp cho hệ thống hi-fi cổ điển.",
 		price: 9900000,
@@ -422,7 +422,7 @@ const productData = {
 		type: "Amply"
 	},
 	SP0050: {
-		name: "Sony TCM-150",
+		name: "TCM-150",
 		origin: "Sony",
 		description: "Máy phát cassette nhỏ gọn với thiết kế cổ điển, phù hợp để nghe lại các băng cassette yêu thích.",
 		price: 1890000,
@@ -430,7 +430,7 @@ const productData = {
 		type: "Máy nghe cassette"
 	},
 	SP0051: {
-		name: "Panasonic RQ-2102",
+		name: "RQ-2102",
 		origin: "Panasonic",
 		description: "Máy cassette cầm tay với loa tích hợp, dễ sử dụng và mang đậm phong cách hoài cổ.",
 		price: 2290000,
@@ -438,7 +438,7 @@ const productData = {
 		type: "Máy nghe cassette"
 	},
 	SP0052: {
-		name: "TEAC W-1200",
+		name: "W-1200",
 		origin: "TEAC",
 		description: "Đầu cassette kép hỗ trợ ghi âm và phát lại chất lượng cao, lý tưởng cho người sưu tầm băng từ.",
 		price: 13990000,
@@ -446,7 +446,7 @@ const productData = {
 		type: "Máy nghe cassette"
 	},
 	SP0053: {
-		name: "FiiO CP13",
+		name: "CP13",
 		origin: "FiiO",
 		description: "Máy nghe cassette di động hiện đại với thiết kế retro, mang đến trải nghiệm âm thanh analog chân thực.",
 		price: 2990000,
@@ -454,7 +454,7 @@ const productData = {
 		type: "Máy nghe cassette"
 	},
 	SP0054: {
-		name: "Jensen MCR-100",
+		name: "MCR-100",
 		origin: "Jensen",
 		description: "Máy cassette đa năng tích hợp AM/FM và ghi âm, phù hợp cho nhu cầu giải trí và lưu trữ băng từ.",
 		price: 2590000,
